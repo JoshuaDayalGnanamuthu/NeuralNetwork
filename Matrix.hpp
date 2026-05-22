4ad56187e6388689c1f05dafbbbff299792aad42
JoshuaDayalGnanamuthu <joshuadayal72@gmail.com>
2026-05-21 22:00:23 -0400

    Updated funtion headers to change all size elements to size_t implementation from int

--- Matrix.hpp
+++ Matrix.hpp
@@ -3,32 +3,37 @@
 #include <cmath>
 #include <stdexcept>
 #include <iostream>
+#include <iomanip>
 
 
 class Matrix {
 
 public:
-  int row, column;
+  size_t row, column;
 
-  Matrix ();
+  Matrix(): row(0), column(0) {}
   
-  Matrix(int row, int column): row(row), column(column) {}
+  Matrix(size_t row, size_t column);
+
+  Matrix(size_t row, size_t column, double value);
   
   Matrix(const Matrix &Object);
 
   Matrix transpose() const;
   
-  Matrix& operator=(const Matrix &Object) const
+  Matrix& operator=(const Matrix &Object);
     
   Matrix operator+(const Matrix &Object) const;
 
   Matrix operator-(const Matrix &Object) const;
-
-  Matrix operator-() const;
   
   Matrix operator*(const Matrix &Object) const;
   
-  friend std::ostream& operator<<(std::ostream &os, const Matrix &Object) const;
+  std::vector<double>& operator[](size_t index);
+
+  const std::vector<double>& operator[](size_t index) const;
+
+  friend std::ostream& operator<<(std::ostream &os, const Matrix &Object);
   
   
   
