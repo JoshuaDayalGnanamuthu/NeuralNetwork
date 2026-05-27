d6da42fbb2ad7d936412291598805949d21b856e
JoshuaDayalGnanamuthu <joshuadayal72@gmail.com>
2026-05-26 22:46:55 -0400

    Implemented initialize parameters method for the Neural Network class

--- NeuralNetwork1.cpp
+++ NeuralNetwork1.cpp
@@ -30,4 +30,11 @@ void NeuralNetwork::leakyrelu(Matrix &Object, double alpha = 0.01) {
   }
 }
 
-      
+/**
+ */
+void NeuralNetwork::initialize_parameters() {
+  for ( size_t i {1}; i < this->layers.size; ++i ) {
+    this->Weights["W" + std::to_string(i)] = Matrix(row = this->layers[i], column =  this->layers[i - 1]);
+    this->basis["b" + std::to_string(i)] = Matrix(row = this->layers[i], column = 1, value = 0);
+  }
+}
